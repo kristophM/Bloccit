13aba5152fc8f06507a36313b532774ecb28e1e8
Kristoph Matthews <krist.m@gmail.com>
2015-06-22 22:14:03 -0400

    Fixed a few bugs with flash and index rendering

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -30,9 +30,13 @@ class QuestionsController < ApplicationController
 
   def update
     @question = Question.find(params[:id])
+    # { :i_want_to_be_admin => true, :question => { title: "hi", body: "hello", resolved: true, is_admin: true } }
+    # { title: "hi", body: "hello", resolved: true }
     if @question.update_attributes(params.require(:question).permit(:title,:body,:resolved))
       flash[:notice] = "The question was successfully updated."
-      redirect_to @question
+      redirect_to @question # HTTP 302 Redirect, Location: /question/123
+      # redirect_to question_path(@question, method: :get, action: :show) # /questions/123
+      # redirect_to question_path(@question, method: :get, action: :edit) # /questions/123
     else
       flash[:error] = "The question could not be updated. Please try again."
       render :edit
@@ -43,10 +47,12 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     if @question.destroy
       flash[:notice] = "This question was successfully deleted."
-      render :index
+      
+      # render :index #HTTP 200 . This would not give the data it needed v a redirect_to, which invokes the index action and loads the @questions instance variable. 
     else
       flash[:error] = "The question could not be deleted. Please try again."
     end
+    redirect_to questions_path
   end
 
 
