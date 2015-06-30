4dcc1ff0c3e88bc81b8b63728cba3f90ebd3e653
Kristoph Matthews <krist.m@gmail.com>
2015-06-29 23:08:15 -0400

    db migration for topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623022252) do
+ActiveRecord::Schema.define(version: 20150630030153) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20150623022252) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +55,14 @@ ActiveRecord::Schema.define(version: 20150623022252) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,6 +83,7 @@ ActiveRecord::Schema.define(version: 20150623022252) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
