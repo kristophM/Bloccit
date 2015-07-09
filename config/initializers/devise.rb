ba611f04b7d53ceef7caffe494aa13777eb9af4b
Kristoph Matthews <krist.m@gmail.com>
2015-07-08 22:56:17 -0400

    prepared ActionMailer settings for production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  #Email
+  config.mailer_sender = "krist.m@gmail.com"
 end
