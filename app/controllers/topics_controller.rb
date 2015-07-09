3e61263fbdab9b6d95359c7af646d8a8f4b139f6
Kristoph Matthews <krist.m@gmail.com>
2015-07-08 23:50:38 -0400

    complete interlude

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -20,7 +20,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new(params.require(:topic).permit(:name, :description))
+    @topic = Topic.new(topic_params)
     authorize @topic
     if @topic.save
       flash[:notice] = "Topic was saved."
@@ -34,13 +34,18 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(params.require(:topic).permit(:name, :description))
+    if @topic.update_attributes(topic_params)
       flash[:notice] = "Topic was updated."
       redirect_to @post 
     else
       flash[:error] = "There was an error saving the topic. Please try again."
       render :edit
     end
-    
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name, :description)
   end
 end
