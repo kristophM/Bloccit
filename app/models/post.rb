5f5e5c97d2e5e7c09764e4bfa04c9c239f603be9
Kristoph Matthews <krist.m@gmail.com>
2015-07-20 15:04:51 +0200

    checkpoint: add post images uploading capabilitiy

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   has_one :summary
+  mount_uploader :postimage, PostimageUploader
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
