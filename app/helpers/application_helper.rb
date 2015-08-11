03f1f5fc113b14ab368e0fbd056bb681ddc15eee
Kristoph Matthews <krist.m@gmail.com>
2015-08-10 22:14:23 -0400

    added application helpers for up and down voting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,25 @@ module ApplicationHelper
        content_tag :div, capture(&block), class: 'form-group'
      end
   end
+
+  def up_vote_link_classes(post)
+    default_class = 'glyphicon glyphicon-chevron-up'
+    if current_user.voted(post) && current_user.voted(post).up_vote?
+      "#{default_class} voted"
+    else
+      "#{default_class}"
+    end
+  end
+
+  def down_vote_link_classes(post)
+    default_class = 'glyphicon glyphicon-chevron-down'
+    if current_user.voted(post) && current_user.voted(post).down_vote?
+      "#{default_class} voted"
+    else
+      "#{default_class}"
+    end
+  end
+
 end
 
 #available in views
