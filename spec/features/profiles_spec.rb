bd2ffaec065b37009613d7d4a3a21f5f13ff198b
Kristoph Matthews <krist.m@gmail.com>
2015-08-13 21:38:40 -0400

    added feature spec for testing a user visiting their own profile

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
+
  describe "Visiting profiles" do
 
    include TestFactories
+   include Warden::Test::Helpers
+   Warden.test_mode!
 
    before do
      @user = authenticated_user
@@ -23,4 +26,25 @@ require 'rails_helper'
      end
 
    end
+
+   before do
+     @user = authenticated_user
+     login_as(@user, :scope => :user)
+     @post = associated_post(user: @user)
+     @comment = Comment.new(user: @user, post: @post, body: "A Comment")
+     allow(@comment).to receive(:send_favorite_emails)
+     @comment.save!
+   end
+
+   describe "user signed in" do
+     it "shows profile" do
+       visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
+       expect( page ).to have_content(@user.name)
+       expect( page ).to have_content(@post.title)
+       expect( page ).to have_content(@comment.body)
+       Warden.test_reset! 
+     end
+   end
+
  end
