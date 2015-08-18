741a19da847533b1901eea73e40c031dd1f602b6
Kristoph Matthews <krist.m@gmail.com>
2015-08-18 10:15:34 -0400

    made profiles_spec.rb more DRY

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -16,7 +16,6 @@ require 'rails_helper'
    end
 
    describe "not signed in" do
-
      it "shows profile" do
        visit user_path(@user)
        expect(current_path).to eq(user_path(@user))
@@ -24,26 +23,17 @@ require 'rails_helper'
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
      end
-
-   end
-
-   before do
-     @user = authenticated_user
-     login_as(@user, :scope => :user)
-     @post = associated_post(user: @user)
-     @comment = Comment.new(user: @user, post: @post, body: "A Comment")
-     allow(@comment).to receive(:send_favorite_emails)
-     @comment.save!
    end
 
    describe "user signed in" do
      it "shows profile" do
+       login_as(@user, :scope => :user)
        visit user_path(@user)
        expect(current_path).to eq(user_path(@user))
        expect( page ).to have_content(@user.name)
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
-       Warden.test_reset! 
+       Warden.test_reset!
      end
    end
 
